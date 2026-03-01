7f72bd831949778b23f0ca607dfbac7e676bdeb8
rohanmoura <rsmoura29@gmail.com>
2026-03-01 19:30:25 +0530

    Supabase Initialize

--- arbitrapay/app/(tabs)/_layout.tsx
+++ arbitrapay/app/(tabs)/_layout.tsx
@@ -24,12 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+  name="test"
+  options={{ title: 'Test' }}
+/>
     </Tabs>
   );
 }
