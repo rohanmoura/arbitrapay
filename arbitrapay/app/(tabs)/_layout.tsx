c4ee6bf3af3a321e40855b610cb8e290503842f9
rohanmoura <rsmoura29@gmail.com>
2026-03-02 11:25:49 +0530

    Code Updated

--- arbitrapay/app/(tabs)/_layout.tsx
+++ arbitrapay/app/(tabs)/_layout.tsx
@@ -23,10 +23,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-  name="test"
-  options={{ title: 'Test' }}
-/>
     </Tabs>
   );
 }
